600b73c005e34da76efbda41f945371c79431da1
davidguzmanp <43582853+davidguzmanp@users.noreply.github.com>
2021-09-24 15:27:12 +0200

    weather missing values

--- Rstudio code/WeatherMissingValuesV2.R
+++ Rstudio code/WeatherMissingValuesV2.R
@@ -5,37 +5,39 @@ library(sqldf)
 library(ggplot2)
 library(ggfortify)
 library(lubridate)
+library(sf)
 
+#Source functions
 source("~/GitHub/BachelorThesis/Rstudio code/Functions.R", encoding = 'UTF-8')
 
-#Filter datas
+
+#Filter datas with registry
 registry <- get_ARPA_Lombardia_AQ_registry()
 
 IDStat <- registry %>% 
   filter(Pollutant%in% c("Ammonia","PM10","PM2.5"),
          is.na(DateStop),
          year(DateStart)<=2017) %>%
-  distinct(IDSensor) %>% pull() %>% sort() # Stations that measure all three since 2017 and haven't been decommissioned
+  distinct(IDSensor) %>% pull() %>% sort() # Stations that measure all three since 2017 and haven't been decomessioned
 
 RegistryRed <- registry %>% 
   filter(Pollutant%in% c("Ammonia","PM10","PM2.5"),
          IDSensor%in% IDStat) 
 
-bestcentralines <- RegistryRed %>% 
+beststations <- RegistryRed %>% 
   group_by(IDStation) %>% 
   summarise(n=n()) %>%
   filter(n>=2) %>% 
   distinct(IDStation) %>%
   pull() # Stations that measure at least 2 of the variables at the same time
 
-
 startyear <- 2018
 lastyear  <- 2020
 
-#PART 1: looking for centralines with 2 or more pollutants
+#PART 1: looking for stations with 2 or more pollutants using SQL queries
 #Starting with the loop for downloading the data + casting of the data
 
-cast <- Download(startyear, lastyear, bestcentralines)
+cast <- Download(startyear, lastyear, beststations)
 
 #Queries for counting the amount of missing datas
 tableMissingAmmmonia<-NULL
@@ -72,16 +74,17 @@ for(index in startyear:lastyear) {
                                   on ma.IDStation = mtodos.IDStation
                                 ')
   name = paste("Missing",index,".csv",sep="" )
+  setwd("/Users/marcovinciguerra/Github/GitTesi/DownloadData/MissingTables")
   write_csv(tableMissingDatasTotal[[index-startyear+1]], name)
 }
 
-#PART 2: looking for centralines with 1 or more pollutants
+#PART 2: looking for stations with 1 or more pollutants
 CentralineMorethan1 <- RegistryRed %>% 
   group_by(IDStation) %>% 
   summarise(n=n()) %>%
   filter(n>=1) %>% 
   distinct(IDStation) %>%
-  pull() # Stations that measure at least 1 of the variables at the same time
+  pull() # Stations that measure at least 2 of the variables at the same time
 
 cast2 <- Download(startyear, lastyear, CentralineMorethan1)
 
@@ -120,6 +123,37 @@ for(index in startyear:lastyear) {
                                   on ma.IDStation = mtodos.IDStation
                                 ')
 }
+
+
+#COUNT OF THE TOTAL OF THE MISSING DATAS for every station
+totalMissingFromBeginning <- NULL
+temp <- NULL
+for(index in 1:(length(tableMissingDatasTotal2))) {
+  if(index==1) {
+    totalMissingFromBeginning <- tableMissingDatasTotal2[[index]]
+  } else {
+    temp <- tableMissingDatasTotal2[[index]]
+    auxiliaryTable <- totalMissingFromBeginning 
+    auxiliaryTable
+    totalMissingFromBeginning <- sqldf('SELECT t.IDStation,t.NameStation, 
+                                      SUM(t.MissingAmmonia+a.MissingAmmonia)  
+                                      as MissingAmmonia,
+                                      SUM(t.MissingPM10+a.MissingPM10)
+                                      as MissingPM10 ,
+                                      SUM(t.MissingPM25+a.MissingPM25)
+                                      as MissingPM25,
+                                      SUM(t.MissingAllThree+a.MissingAllThree)
+                                      as t.MissingAllThree
+                                      FROM temp t 
+                                      JOIN auxiliaryTable a
+                                      ON t.IDstation = a.IDStation
+                                      GROUP BY t.IDStation
+                                       ')
+  } 
+}
+
+
+
 
 #YES/NO TABLE
 #Creating the table of yes/no
@@ -173,7 +207,7 @@ presencetable <- sqldf('SELECT IDStation, NameStation, PM25, PM10, Ammonia
 
 #Plot of the centralines with at least 1 observation for one of the pollutant
 presencetable <- presencetable %>%
-  mutate(Etichetta = case_when(PM10 == 1 & PM25 == 1 & Ammonia == 1 ~ "Tutti",
+  mutate(Tag = case_when(PM10 == 1 & PM25 == 1 & Ammonia == 1 ~ "All",
                                PM10 == 1 & PM25 == 1 & Ammonia == 0 ~ "PM10-PM2.5",
                                PM10 == 1 & PM25 == 0 & Ammonia == 1 ~ "PM10-NH3",
                                PM10 == 0 & PM25 == 1 & Ammonia == 1 ~ "PM2.5-NH3",
@@ -182,14 +216,14 @@ presencetable <- presencetable %>%
                                PM10 == 0 & PM25 == 0 & Ammonia == 1 ~ "NH3"))
 
 presencetable_red <- presencetable %>%
-  select(IDStation,Etichetta)
+  select(IDStation,Tag)
 
 RegistryRed <- full_join(RegistryRed,presencetable_red,by = c("IDStation"))
 
 write.table(presencetable, "presencetable_red.csv")
 
 #PART 3: plot of the Lombardy map
-map_Lombardia_stations_custom(RegistryRed,col_points = Etichetta)
+map_Lombardia_stations_custom(RegistryRed,col_points = Tag)
 
 #win.graph()
 map_Lombardia_stations_custom(RegistryRed)
@@ -255,7 +289,6 @@ for (i in 1:length(tableMissingAmmmonia)) {
 
 
 threeYesPlot <- NULL
-#setwd("/Users/marcovinciguerra/Github/GitTesi/DownloadData/Dataplot")
 
 for (i in 1:length(presencetableYear)) {
   
@@ -325,18 +358,24 @@ for (i in 1:length(lastYearStations)) {
 
 BlueStripes(FullStations,"2018-2020")
 
-# Distances
-
-
 #PART 5: Nearest Neighbor
 #Calculate the distances of the 2 nearest stations 
 regAQ <- get_ARPA_Lombardia_AQ_registry()
+
 regAQ <- regAQ %>%
   filter(Pollutant %in% c("PM10"), is.na(DateStop)) %>%
   distinct(IDStation,NameStation,Longitude,Latitude) %>%
   mutate(lng = Longitude, lat = Latitude) %>%
   sf::st_as_sf(coords = c("lng", "lat"), crs=4326)
+
 regW <- get_ARPA_Lombardia_W_registry()
+
+regW <- regW %>% 
+  filter(Measure %in% c("Wind_speed","Wind_direction","Temperature","Rainfall"),
+         is.na(DateStop),
+         year(DateStart)<=2017) 
+
+
 regW <- regW %>%
   filter(is.na(DateStop)) %>%
   distinct(IDStation,NameStation,Longitude,Latitude) %>%
@@ -374,6 +413,7 @@ tableAllyears <- get_ARPA_Lombardia_AQ_data(
   Frequency = "daily",
 )
 
+we <- data.frame(we)
 tableAllyears <- data.frame(tableAllyears)
 
 #Table with pollution stations and the first nearest weather station 
@@ -381,9 +421,27 @@ aqwe<- sqldf('select *
       from tableAllyears t join equiv e on t.IDStation = e.IDStation join we on e.reg_Y_nn1_ID = we.IDStation
                where t.Date = we.Date')
 
+
 write_csv(aqwe,'NNdata.csv')
 
 # single out the weather variables to study regarding their missing values
+
+FullStationsW <- NULL
+
+WStations <- sqldf('select distinct IDStation
+                   from we')
+WStations <- as.matrix(WStations)
+
+for (i in 1:length(WStations)) {
+  
+  FullStationsW[[i]] <- sqldf(paste("SELECT *
+                             FROM we
+                             WHERE IDStation = ", WStations[i],sep = ""))
+  
+}
+
+
+BlueStripesW(FullStationsW,"2018-2020")
 
 # part 6: Scatterplots
 
@@ -397,4 +455,3 @@ df$color <- factor(df$NameStation,
                    labels=c("blue", "red","green","orange","coral","brown"))
 
 plot(df[,c('Ammonia','PM10','PM25')], pch = 16,  col = alpha(as.character(df$color),0.45))
-
