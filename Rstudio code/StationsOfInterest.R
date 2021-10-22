0fc1d5cd0768b3a1e7ba69f91a97e4f6e96717e0
davidguzmanp <43582853+davidguzmanp@users.noreply.github.com>
2021-10-22 15:21:19 +0200

    Update StationsOfInterest.R

--- Rstudio code/StationsOfInterest.R
+++ Rstudio code/StationsOfInterest.R
@@ -290,13 +290,19 @@ for (i in 1:ncol(MissingCount)) { # saving all piecharts
 
 #5 Unique table for AQ and W--------------------------------------------
 
-aq <- Easydownload(2018,2020,681)
+aq <- Easydownload(startyear,endyear,arrayStations)
 w <-  get_ARPA_Lombardia_W_data(
-  ID_station = distanceConstrained[distanceConstrained[,'IDStation']==681,'reg_Y_nn1_ID'], 
+  ID_station = distanceConstrained[distanceConstrained[,'IDStation']==arrayStations,'reg_Y_nn1_ID'], 
   Year = c(startyear:endyear),
   Frequency = "daily")
 
-equiv <- distance[,c(1,6)]
+w <-  get_ARPA_Lombardia_W_data(
+  ID_station = c(677 , 671, 111), 
+  Year = c(startyear:endyear),
+  Frequency = "daily")
+
+
+equiv <- distanceConstrained[,c(1,6)]
 
 aqw<- sqldf('select *
       from aq t join equiv e on t.IDStation = e.IDStation join w on e.reg_Y_nn1_ID = w.IDStation
@@ -305,18 +311,4 @@ aqw<- sqldf('select *
 
 write_csv(aqw,'NNdata.csv')
 
-aq <- Easydownload(2018,2020,681)
-w <-  get_ARPA_Lombardia_W_data(
-  ID_station = distanceConstrained[distanceConstrained[,'IDStation']==681,'reg_Y_nn1_ID'], 
-  Year = c(startyear:endyear),
-  Frequency = "daily")
-
-equiv <- distance[,c(1,6)]
-
-aqw<- sqldf('select *
-      from aq t join equiv e on t.IDStation = e.IDStation join w on e.reg_Y_nn1_ID = w.IDStation
-               where t.Date = w.Date')
-
-
-write_csv(aqw,'NNdata.csv')
 
